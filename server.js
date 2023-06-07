9ffa4b3685dc131f5c6255944576df79c3b8de71
nicolas-escolasorvete <nicolas@escolasorvete.com.br>
2023-06-07 16:44:27 -0300

    jsondatasave

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mailchimp = require('@mailchimp/mailchimp_marketing');
 const cors = require('cors');
 require('dotenv').config();
+const { uuid } = require('uuidv4');
 
 const app = express();
 const PORT = 8000;
@@ -36,14 +37,60 @@ app.post('/api/mailchimp', cors(corsOptions), async (req, res) => {
   };
 
   try {
-    const response = await mailchimp.lists.addListMember(listId, subscriber);
-    console.log('Miembro agregado:', response);
+//    const response = await mailchimp.lists.addListMember(listId, subscriber);
+//    console.log('Miembro agregado:', response);
     res.status(200).json({ message: 'Miembro agregado correctamente' });
   } catch (error) {
     console.error('Error al agregar el miembro:', error);
     res.status(500).json({
       message: 'Error al agregar el miembro',
       error: error });
+  } finally {
+
+
+    const fs = require('fs');
+
+    const newSub = {
+      id: uuid(),
+      name: email,
+      email: firstName,
+      phone: lastName
+    }
+
+
+    // Read the existing JSON file
+    fs.readFile('mailchimp-data.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error('Error reading file:', err);
+        return;
+      }
+
+  try {
+        // Parse the JSON data into an array or object
+    const jsonData = JSON.parse(data);
+
+    console.log(jsonData)
+
+
+    // Add the new object to the JSON data
+    jsonData.push(newSub);
+
+    // Convert the modified JSON data back to a string
+    const updatedData = JSON.stringify(jsonData, null, 2);
+
+    // Write the updated JSON data back to the file
+    fs.writeFile('mailchimp-data.json', updatedData, 'utf8', (err) => {
+      if (err) {
+        console.error('Error writing file:', err);
+        return;
+      }
+      console.log('Object added successfully!');
+    });
+      } catch (error) {
+        console.error('Error parsing JSON data:', error);
+      }
+    });
+
   }
 });
 
